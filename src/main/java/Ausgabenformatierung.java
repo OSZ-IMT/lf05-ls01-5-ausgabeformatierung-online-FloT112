185e5b24d250597107ade6f6c3ff2a3d4733b3b1
FloT112 <Dasfaddl@gmail.com>
2020-09-10 08:23:16 +0000

    code

--- src/main/java/Ausgabenformatierung.java
+++ src/main/java/Ausgabenformatierung.java
@@ -25,13 +25,38 @@ public class Ausgabenformatierung {
 
     public static void aufgabe1(){
 
+      System.out.println("   **   ");
+      System.out.println("*      *");
+      System.out.println("*      *");
+      System.out.println("   **   ");
+      
+
     }
 
     public static void aufgabe2(){
-
+      System.out.printf("%-5s = %-18s = %4d\n", "0!", "", 1);
+      System.out.printf("%-5s = %-18s = %4d\n", "1!", "1", 1);
+      System.out.printf("%-5s = %-18s = %4d\n", "2!", "1 * 2", 2);
+      System.out.printf("%-5s = %-18s = %4d\n", "3!", "1 * 2 * 3", 6);
+      System.out.printf("%-5s = %-18s = %4d\n", "4!", "1 * 2 * 3 * 4", 24);
+      System.out.printf("%-5s = %-18s = %4d\n", "5!", "1 * 2 * 3 * 4 * 5", 120);
     }
 
     public static void aufgabe3(){
+
+      String a = "Fahrenheit";
+      String b = "Celsius";
+
+      System.out.printf("%-12s|%10s", a, b);
+      System.out.printf("\n----------------------\n");
+
+      int hi=-20;
+
+      while (hi<=30){
+        double celsius = (5.0/9.0) * (hi - 32.0);
+        System.out.printf("%+-12d|%18.2f\n", hi, celsius);
+        hi += 10;
+      }
 
     }
 }
